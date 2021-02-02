9bc9f378ecfa5e92ae0b7115084227cfa81d1311
Eriberto Guzman <eriberto.guzman@flatironschool.com>
2021-02-02 20:13:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+const person = {
+  name: "Terrance Roberts",
+  occupation: {
+    title: "District Manager",
+    yearsHeld: 2
+  },
+  pets: [{
+    kind: "dog",
+    name: "Fiona"
+  },{
+    kind: "Cat",
+    name: "Ralph:"
+  }]
+}
+console.log(person)
